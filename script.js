0f6939bc927baa900301285f0a65276973052c5c
Edin <edinosmic77@gmail.com>
2021-12-01 08:45:25 +0100

    menu commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Nav
+function openNav() {
+    document.getElementById('myNav').style.width = '100%'
+}
+
+function closeNav() {
+    document.getElementById('myNav').style.width = '0%'
+}
+
+// Login
+function openNavLogin() {
+    document.getElementById('myNavLogin').style.width = '100%'
+}
+
+function closeNavLogin() {
+    document.getElementById('myNavLogin').style.width = '0%'
+}
+
+// Help
+function openNavHelp() {
+    document.getElementById('myNavHelp').style.width = '100%'
+}
+
+function closeNavHelp() {
+    document.getElementById('myNavHelp').style.width = '0%'
+}
+
+// Languages
+function openNavLanguages() {
+    document.getElementById('myNavLanguages').style.width = '100%'
+}
+
+function closeNavLanguages() {
+    document.getElementById('myNavLanguages').style.width = '0%'
+}
+
+// Profile
+function openNavProfile() {
+    document.getElementById('myNavProfile').style.width = '100%'
+}
+
+function closeNavProfile() {
+    document.getElementById('myNavProfile').style.width = '0%'
+}
